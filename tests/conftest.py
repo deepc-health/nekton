80c7d6d933ae59b038cfee323d49a6d46bf80cde
aparida <abhijeet@deepc.ai>
2021-12-08 12:30:50 +0000

    ci(ci-helpers): adding basic ci helpers

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+# add root-dir to sys path for tests
+import sys
+from os.path import abspath
+from os.path import dirname as d
+
+parent_dir = f"{d(d(abspath(__file__)))}"
+sys.path.append(f"{parent_dir}/nekton")
---
+++ tests/test_0_sample.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+@pytest.mark.devtest
+def test_0_1_dummy():
+    pass
