119af06be73aa4950605aeb4595c52fc1023353f
a-parida12 <abhijeet.parida@tum.de>
2022-09-28 14:37:39 -0400

    fix(test): test with new api endpoints

--- tests/test_2_dcm_nii_conversion.py
+++ tests/test_2_dcm_nii_conversion.py
@@ -8,7 +8,7 @@ def test_2_1_check_run_conv_uniform(converter_nii, site_package_path):
         site_package_path, "pydicom/data/test_files/dicomdirtests/98892001/CT5N/"
     )
 
-    output_paths = converter_nii._run_conv_uniform(path_dcms)
+    output_paths = converter_nii._run_conv_uniform(path_dcms, None)
     assert len(output_paths) == 1
     [os.remove(path) for path in output_paths]
 
@@ -16,7 +16,7 @@ def test_2_1_check_run_conv_uniform(converter_nii, site_package_path):
 @pytest.mark.dcm2nii
 def test_2_2_check_end2end_variable(converter_nii):
     with pytest.raises(NotImplementedError):
-        converter_nii._run_conv_variable([])
+        converter_nii._run_conv_variable([], None)
     with pytest.raises(RuntimeError):
         converter_nii.run("tests/test_data/variable_SliceThickness")
 
@@ -44,7 +44,7 @@ def test_2_3_check_end2end_uniform_rename(converter_nii, site_package_path):
         site_package_path, "pydicom/data/test_files/dicomdirtests/98892001/CT5N/"
     )
 
-    output_paths = converter_nii.run(path_dcms, "new_name")
+    output_paths = converter_nii.run(path_dcms, None,  "new_name")
     for path in output_paths:
         assert os.path.exists(path)
         os.remove(path)
